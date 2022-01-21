c667ef329f58d9f786fde3cdc6dc2d39ec29d686
Thunderwraith <ilya.botvinko@gmail.com>
2022-01-21 15:58:11 +0200

    bot test

--- bot.js
+++ bot.js
@@ -4,11 +4,10 @@ const TelegramBot = require('node-telegram-bot-api')
 
 const key = process.env.KEY
 
-console.log(key)
-
 const bot = new TelegramBot(key, {polling: true})
 
 bot.on('message', (msg) => {
+  console.log(msg)
   const chatId = msg.chat.id
-  bot.sendMessage(chatId, `Привет, ${msg.chat.username}! чего хочешь? ${msg.chat.id}`)
+  bot.sendMessage(chatId, `Привет, ${msg.chat.username}! *MESSAGE = ${msg.text}*`)
 })
